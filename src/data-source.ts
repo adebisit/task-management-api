805e639172ac36df01b8897ff57fd9a0d59d3aeb
adebisit <adebisijosephh@gmail.com>
2024-05-22 06:51:57 +0100

    Update Proc & DB_HOST

--- src/data-source.ts
+++ src/data-source.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'mysql',
-  host: 'mysql_db',
+  host: process.env.DB_HOST || 'mysql_db',
   port: parseInt(process.env.DB_PORT, 10) || 3306,
   username: process.env.DB_USERNAME || 'root',
   password: process.env.DB_PASSWORD,
